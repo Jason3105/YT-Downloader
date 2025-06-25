efe09497f6286cf2a828f285d96d2e3aca331563
Jason3105 <jasongons312005@gmail.com>
2025-06-25 20:10:40 +0530

    cleanup and rebuild after tailwind fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
+// filepath: d:\YT-Downloader\YT-Downloader\frontend\vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
