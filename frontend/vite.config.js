d9770200342b3af01d38d14fbac7d1bf15983306
Jason3105 <jasongons312005@gmail.com>
2025-06-26 00:31:30 +0530

    Updates

--- frontend/vite.config.js
+++
@@ -1,7 +0,0 @@
-// filepath: d:\YT-Downloader\YT-Downloader\frontend\vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-})
